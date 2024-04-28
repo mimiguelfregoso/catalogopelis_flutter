0aa2c8fff69e03d08e66daad92a24f125380bab5
mimiguelfregoso <91863992+mimiguelfregoso@users.noreply.github.com>
2024-04-28 14:00:39 -0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,143 +1,65 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
-  runApp(CatalogoPelisApp());
+  runApp(MyApp());
 }
 
-class CatalogoPelisApp extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'CatalogoPelis',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: CatalogoPelisScreen(),
+      home: MovieCatalog(),
     );
   }
 }
 
-class CatalogoPelisScreen extends StatelessWidget {
-  final List<Pelicula> peliculas = [
-    Pelicula('Inception', 'A thief who enters the dreams of others to steal their secrets.'),
-    Pelicula('Interstellar', 'A team of explorers travel through a wormhole in space in an attempt to ensure humanity\'s survival.'),
-    Pelicula('The Shawshank Redemption', 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.'),
-    // Puedes agregar más películas aquí
-  ];
+class MovieCatalog extends StatefulWidget {
+  @override
+  _MovieCatalogState createState() => _MovieCatalogState();
+}
+
+class _MovieCatalogState extends State<MovieCatalog> {
+  List<dynamic> movies = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchMovies();
+  }
+
+  Future<void> fetchMovies() async {
+    final response = await http.get(Uri.parse('URL_DE_TU_API'));
+
+    if (response.statusCode == 200) {
+      setState(() {
+        movies = json.decode(response.body);
+      });
+    } else {
+      throw Exception('Failed to load movies');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('BIENVENIDO A CatalogoPelis - Catálogo de Películas'),
-      ),
-      body: Stack(
-        children: [
-          // Fondo de pantalla
-          Container(
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage('assets/images/fondo_pelis.jpg'), // AQUI VA LA IMAGEN DE FONDO
-                fit: BoxFit.cover,
-              ),
-            ),
-          ),
-          // CONTENIDOS
-          Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                // MSG
-                Text(
-                  '¡Bienvenido a CatalogoPelis!',
-                  style: TextStyle(
-                    fontSize: 24.0,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                  ),
-                ),
-                SizedBox(height: 20.0),
-                // NOMBRE DE LA APLICACION
-                Text(
-                  'CatalogoPelis',
-                  style: TextStyle(
-                    fontSize: 36.0,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                  ),
-                ),
-                SizedBox(height: 40.0),
-                // SE PONE BOTON PARA VER CATALOGO
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => CatalogoPage(peliculas: peliculas)),
-                    );
-                  },
-                  child: Text(
-                    'Ver catálogo',
-                    style: TextStyle(fontSize: 20),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class Pelicula {
-  final String titulo;
-  final String descripcion;
-
-  Pelicula(this.titulo, this.descripcion);
-}
-
-class CatalogoPage extends StatelessWidget {
-  final List<Pelicula> peliculas;
-
-  CatalogoPage({required this.peliculas});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Catálogo de Películas'),
+        title: Text('Movie Catalog'),
       ),
       body: ListView.builder(
-        itemCount: peliculas.length,
+        itemCount: movies.length,
         itemBuilder: (context, index) {
-          return Padding(
-            padding: EdgeInsets.all(8.0),
-            child: Container(
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.blue),
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              child: ListTile(
-                title: Text(
-                  peliculas[index].titulo,
-                  style: TextStyle(
-                    fontSize: 18.0,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                subtitle: Text(
-                  peliculas[index].descripcion,
-                  style: TextStyle(
-                    fontSize: 16.0,
-                  ),
-                ),
-              ),
-            ),
+          return ListTile(
+            title: Text(movies[index]['title']),
+            subtitle: Text(movies[index]['description']),
           );
         },
       ),
     );
+  }
+}
+
   }
 }
 
